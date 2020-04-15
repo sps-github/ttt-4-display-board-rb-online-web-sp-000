561a37817af3b00a3e81755745db37b9c05d55a8
Sarah Peters <sarahpieters15@gmail.com>
2020-04-15 15:35:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,11 @@ def display_board
   puts "-----------"
   puts "   |   |   "
 end
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
