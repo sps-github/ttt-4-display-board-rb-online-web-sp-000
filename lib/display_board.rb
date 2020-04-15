45f9d07909a1e2d90b8292f894c1671006f05dbc
Sarah Peters <sarahpieters15@gmail.com>
2020-04-15 16:12:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,8 +6,7 @@ def display_board
   puts "   |   |   "
 end
 
-def display_board(board)
-  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}"
-end
+board = [" "," "," "," "," "," "," "," "," "]
 
-board[0] = [" "," "," "," ","X"," "," "," "," "]
+def display_board(board)
+end
