cec188614bea8f786970f97c7eef4c9ccd188502
Sarah Peters <sarahpieters15@gmail.com>
2020-04-15 16:31:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,5 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+board = "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}"
